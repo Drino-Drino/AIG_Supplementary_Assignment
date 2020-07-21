1a6c3832407efb420f00c0dbd4bb3e7b3296ef29
Andreas Kudumo <akudumo@mme.gov.na>
2020-07-21 23:50:21 +0200

    Various Syntax and display changes. World Initialization function added

--- AIG_supp_Ass.jl
+++ AIG_supp_Ass.jl
@@ -3,9 +3,10 @@ using Pkg
 Pkg.add("DataStructures") #for stacks and queues
 using DataStructures
 
+
 function worldDefinition() #A function that defines the objects in the Blocks world
 #Specifying the objects to be moved
-println("How many stored objects are there? (These are objects to be moved/stored)")
+println("How many stored objects are there? (These are objects to be moved/stored, e.g Blocks)")
 numberOfBlocks = parse(Int, readline())
 
 blocksArray = Array{String}(undef, numberOfBlocks)
@@ -14,7 +15,7 @@ for i in 1:numberOfBlocks
     blocksArray[i] = readline()
 end
 
-println("How many storage objects are there? (These are objects that carry/store the stored objects)")
+println("How many storage objects are there? (These are objects that carry/store the stored objects, e.g a box or table)")
 numberOfstorObjects = parse(Int, readline())
 
 storageObjArray = Array{String}(undef, numberOfstorObjects)
@@ -26,18 +27,20 @@ for i in 1:numberOfstorObjects
     storageLimit[i] = parse(Int, readline())
 end
 
-actualObjContentArray = Array{Array}(undef, numberOfstorObjects)
+actualObjContentArray = Array{Array}(undef, numberOfstorObjects) #Define the world as array of carrier objects
 
-#Assign Array objetcs within
+#Defining storage spaces as stacks
 for i in 1:numberOfstorObjects
     for j in 1:numberOfBlocks
-    actualObjContentArray[i][j] = Stack{String}()
-    #Array{Stack}(undef, numberOfBlocks)
+    actualObjContentArray[i] = Array{Stack}(undef, numberOfBlocks) #define carrier objects as arrays with the capacity of the number of blocks
+    actualObjContentArray[i][j] = Stack{String}() #define each storage position in all carrier objects as a stack
+
 end
 end
 end
 
-#function worldInitial () #A function to initialize the world
+
+function worldInitial() #A function to initialize the world
 
 for i in 1:numberOfstorObjects
     for j in 1:numberOfBlocks
@@ -46,8 +49,24 @@ for i in 1:numberOfstorObjects
 end
 end
 
+println("State Objects from bottom to top (Where objects are on top of each other)")
+for i in 1:numberOfBlocks
+println("Where is ", blocksArray[i])
+index = findall(x->x== readline(), storageObjArray)
+println("Is it on top of another stored object? (e.g another block) Reply Y or N")
+verticalPos = readline()
+if (verticalPos = "Y")
+    println("What object is it lying on?"
+    bottomIndex = findall(x->x== readline(), actualObjContentArray)
+    push!(actualObjContentArray[bottomIndex], blocksArray[i])
+else
+    push!(actualObjContentArray[index][i], "Block A")
+end
+end
+end
+
 #Show user current configuration of storage/carrier objects
-displayDict = Dict(
+displayDict = Dict(storageObjArray[1]
 
 
 
